5328d0dea7f73c796bdb78470d60286ff997ebd8
Charlie Prezzano <charlie@hawthornethreads.com>
2025-01-22 18:10:45 -0500

    hmwk2 - started screentime

---
+++ DaddyTime/CP116/Homework-2/screentime.py
@@ -0,0 +1,34 @@
+# **************************************************************************
+
+def find_all_names(min_lines):
+    return 'Elsinore. Ber. Fran. Hor. Mar. King. Laer. Pol. Ham. Queen. Both. All. Oph. Exeunt. Ghost. Rey. Ros. Guil. 1. Capt. Clown. Other. Osr.'
+
+# **************************************************************************
+
+def find_nbr_lines_for_name(name):
+    return 45
+
+# **************************************************************************
+
+print('I just finished reading Hamlet, and can tell you how many lines each character spoke!')
+print('Please input one of the three following commands:')
+print('        "names": display all character names')
+print('        "exit":  terminates the program')
+print('        "{name}": display how many lines were spoken by that character')
+print('')
+
+command = input('Enter command: ')
+while command != 'exit':
+    if command == 'names':
+        names = find_all_names(5)
+        print('Here are the valid character names (with > 5 lines): ' + names)
+    elif command == 'exit':
+        print('Okay, shutting down.')
+    else:
+        name = command
+        nbr_lines = find_nbr_lines_for_name(name)
+        print('Based on my count, ' + name + ' had ' + str(nbr_lines) + ' lines.')
+    print('')
+    command = input('Enter command: ')
+
+# **************************************************************************
