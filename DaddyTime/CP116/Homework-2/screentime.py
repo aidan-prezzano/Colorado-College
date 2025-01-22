704d5757c12ebc3c60116516585d1548482e018b
Charlie Prezzano <charlie@hawthornethreads.com>
2025-01-22 18:23:46 -0500

    hmwk2 - fixed screentime ... checked that name to find # lines for is apart of names

--- DaddyTime/CP116/Homework-2/screentime.py
+++ DaddyTime/CP116/Homework-2/screentime.py
@@ -1,7 +1,7 @@
 # **************************************************************************
 
 def find_all_names(min_lines):
-    return 'Elsinore. Ber. Fran. Hor. Mar. King. Laer. Pol. Ham. Queen. Both. All. Oph. Exeunt. Ghost. Rey. Ros. Guil. 1. Capt. Clown. Other. Osr.'
+    return ['Elsinore.', 'Ber.', 'Fran.', 'Hor.', 'Mar.', 'King.', 'Laer.', 'Pol.', 'Ham.', 'Queen.', 'Both.', 'All.', 'Oph.', 'Exeunt.', 'Ghost.', 'Rey.', 'Ros.', 'Guil.', '1.', 'Capt.', 'Clown.', 'Other.', 'Osr.']
 
 # **************************************************************************
 
@@ -17,17 +17,22 @@ print('        "exit":  terminates the program')
 print('        "{name}": display how many lines were spoken by that character')
 print('')
 
+names = find_all_names(5)
+names_str = str(names).replace('[', '').replace(']', '').replace('\'', '')
+
 command = input('Enter command: ')
 while command != 'exit':
     if command == 'names':
-        names = find_all_names(5)
-        print('Here are the valid character names (with > 5 lines): ' + names)
+        print('Here are the valid character names (with > 5 lines): ' + names_str)
     elif command == 'exit':
         print('Okay, shutting down.')
     else:
         name = command
-        nbr_lines = find_nbr_lines_for_name(name)
-        print('Based on my count, ' + name + ' had ' + str(nbr_lines) + ' lines.')
+        if name in names:
+            nbr_lines = find_nbr_lines_for_name(name)
+            print('Based on my count, ' + name + ' had ' + str(nbr_lines) + ' lines.')
+        else:
+            print(name + ' is not a valid character name')
     print('')
     command = input('Enter command: ')
 
