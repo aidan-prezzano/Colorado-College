f4266e937bdb313b111041c0f149c132e9e32987
aidanprezzano <aidanprezzano@gmail.com>
2025-01-23 15:52:15 -0700

    updated gitignore

---
+++ CP116 - Applied Python/Homework three- grades/homework_3.py
@@ -0,0 +1,27 @@
+def mainloop():
+    def help_menu():
+
+        # Neatly prints out all valid user inputs and what they do
+        print("'add': add a new student")
+        print("'set': update a student's score for an assignment")
+        print("'display': print out all records in the database")
+        print("'dayavg': view the average score for an assignment (across students)")
+        print("'nameavg': view the average score for a student (across assignments)")
+        print("'quit': exit the program")
+
+        # Parameters:
+        # ... # TODO
+        # Returns:
+        # None
+
+    user_action = input("What action would you like to take? ('help' for options): ")
+
+    while user_action != "quit":
+        if user_action == "help":
+            print(help_menu())
+            user_action = input("What action would you like to take? ('help' for options): ")
+
+    print("See ya later")
+
+
+print(mainloop())
