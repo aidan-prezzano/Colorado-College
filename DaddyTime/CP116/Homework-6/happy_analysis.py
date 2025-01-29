1de9580e99a7503d7f966aa414e33ad990ba5437
Charlie Prezzano <charlie@hawthornethreads.com>
2025-01-28 22:29:20 -0500

    hmwk6

--- DaddyTime/CP116/Homework-6/happy_analysis.py
+++ DaddyTime/CP116/Homework-6/happy_analysis.py
@@ -8,7 +8,16 @@ import pandas as pd
 
 def us_happiness_trend_last_decade(df):
 
-    trend = 'DOWN'
+    # find the US ladder score in 2013
+    us_2013_row = df.loc[(df['Country'] == 'United States') & (df['year'] == 2013)].iloc[0]
+    us_2013_ladder_score = us_2013_row['Life Ladder']
+
+    # find the US ladder score in 2023
+    us_2023_row = df.loc[(df['Country'] == 'United States') & (df['year'] == 2023)].iloc[0]
+    us_2023_ladder_score = us_2023_row['Life Ladder']
+
+    # determine if the happiness trend, based on ladder score, is going UP or DOWN
+    trend = 'UP' if us_2023_ladder_score > us_2013_ladder_score else 'DOWN'
 
     print('The US happiness has gone ' + trend + ' over the last decade')
 
@@ -18,7 +27,16 @@ def us_happiness_trend_last_decade(df):
 
 def global_happiness_trend_last_decade(df):
 
-    trend = 'UP'
+    # find the the ladder score sum of all countries in 2013
+    global_2013_rows = df.loc[df['year'] == 2013]
+    global_2013_ladder_score_sum = global_2013_rows['Life Ladder'].sum()
+
+    # find the the ladder score sum of all countries in 2023
+    global_2023_rows = df.loc[df['year'] == 2023]
+    global_2023_ladder_score_sum = global_2023_rows['Life Ladder'].sum()
+
+    # determine if the happiness trend, based on ladder score, is going UP or DOWN
+    trend = 'UP' if global_2023_ladder_score_sum > global_2013_ladder_score_sum else 'DOWN'
 
     print('Globally, happiness has gone ' + trend + ' over the last decade')
 
--- DaddyTime/CP116/Homework-6/olympic_analysis.py
+++ DaddyTime/CP116/Homework-6/olympic_analysis.py
@@ -1,47 +1,67 @@
 # in terminal, ran: pip install pandas
 
-import pandas
-
-
-# **************************************************************************
-
-def youngest_olympian_to__receive_medal(data_frame):
-
-    athelete = 'Aidan Prezzano'
-    sport = 'Soccer'
-    year = 2020
-
-    print('Youngest olympian to receive a medal was ' + athelete + '. The medal was in ' + sport + ', in the year ' + str(year) + '.')
+import pandas as pd
 
 
 # **************************************************************************
 
 
-def city_with_lowest_nbr_medals_awarded(data_frame):
+def youngest_olympian_to_receive_medal(df):
 
-    city = 'Rhinebeck'
+    # find the minimum age within the dataset
+    # then find the row, at this age
+    min_age = df['Age'].min()
+    row = df.loc[df['Age'] == min_age].iloc[0]
 
-    print('The lowest number of awarded medals took place in ' + city + '.')
+    # from this row get the athletes name, sport, and year that won the medal
+    athlete = row['Name']
+    sport = row['Sport']
+    year = row['Year']
 
-
-# **************************************************************************
-
-
-def who_won_most_silver_medals(data_frame):
-
-    athelete = 'Aidan Prezzano'
-
-    print(athelete + ' has won the most silver medals.')
+    print('Youngest olympian to receive a medal was ' + athlete + '. The medal was in ' + sport + ', in the year ' + str(year) + '.')
 
 
 # **************************************************************************
 
 
-def unique_athletes_to_win_a_medal_after_40(data_frame):
+def city_with_lowest_nbr_medals_awarded(df):
 
-    nbr_athletes = 99
+    # group the dataset by event (year and season)
+    # then find the row with the fewest entries - with the least amount of medals
+    year_season_rows = df.groupby(['Year', 'Season']).size()
+    row = year_season_rows.sort_values(ascending=True).index[0]
 
-    print('The number of (unique) athletes who have won a medal after the age of 40 is: ' + str(nbr_athletes))
+    # from the row get the event year and season
+    event_year = row[0]
+    event_season = row[1]
+
+    print('The lowest number of awarded medals took place in the ' + event_season + ' of ' + str(event_year) + '.')
+
+
+# **************************************************************************
+
+
+def who_won_most_silver_medals(df):
+
+    # find all rows that have a silver medal
+    # then with these rows, order the athletes by count and get the athlete who has the most
+    silver_medal_rows = df[df['Medals'] == 'Silver']
+    athlete = silver_medal_rows['Name'].value_counts().index[0]
+
+    print(athlete + ' has won the most silver medals.')
+
+
+# **************************************************************************
+
+
+def unique_athletes_to_win_a_medal_after_40(df):
+
+    # find all rows that have an age greater than 40
+    # then with these rows, find the unique athletes
+    after_40_rows = df[df['Age'] > 40]
+    unique_athletes_after_40 = after_40_rows['Name'].unique()
+
+    print('The number of (unique) athletes who have won a medal after the age of 40 is: ' + str(len(unique_athletes_after_40)))
 
 
 # **************************************************************************
@@ -49,12 +69,12 @@ def unique_athletes_to_win_a_medal_after_40(data_frame):
 
 def run_olympic_analysis():
 
-    data_frame = pandas.read_csv('olympic_data.csv')
+    df = pd.read_csv('olympic_data.csv')
 
-    youngest_olympian_to__receive_medal(data_frame)
-    city_with_lowest_nbr_medals_awarded(data_frame)
-    who_won_most_silver_medals(data_frame)
-    unique_athletes_to_win_a_medal_after_40(data_frame)
+    youngest_olympian_to_receive_medal(df)
+    city_with_lowest_nbr_medals_awarded(df)
+    who_won_most_silver_medals(df)
+    unique_athletes_to_win_a_medal_after_40(df)
 
 
 # **************************************************************************
