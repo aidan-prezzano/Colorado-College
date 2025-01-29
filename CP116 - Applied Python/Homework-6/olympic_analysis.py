554be72e423a64881f5cd03016f3ffb959f6ad3e
aidanprezzano <aidanprezzano@gmail.com>
2025-01-28 19:56:57 -0700

    updated gitignore

--- CP116 - Applied Python/Homework-6/olympic_analysis.py
+++ CP116 - Applied Python/Homework-6/olympic_analysis.py
@@ -1,16 +1,69 @@
+
 import pandas as pd
 
-def youngest_olympian_medal(df):
-    age_df = df['Age']
-    print(age_df)
 
-    print("the youngest olympian to win a medal is...")
+# **************************************************************************
 
+def youngest_olympian_to__receive_medal(df):
+
+    youngest = df.loc[df['Age'].idxmin(), ['Name', 'Age', 'Sport', 'Year']]
+    print(youngest)
+
+    athelete = 'Luigina Giavotti'
+    sport = 'Gymnastics'
+    year = 1928
+
+    print('Youngest olympian to receive a medal was ' + athelete + '. The medal was in ' + sport + ', in the year ' + str(year) + 'at 11 years old.')
+
+
+# **************************************************************************
+
+
+def city_with_lowest_nbr_medals_awarded(df):
+
+    city = 'Rhinebeck'
+
+    print('The lowest number of awarded medals took place in ' + city + '.')
+
+
+# **************************************************************************
+
+
+def who_won_most_silver_medals(df):
+
+    athelete = 'Aidan Prezzano'
+
+    print(athelete + ' has won the most silver medals.')
+
+
+# **************************************************************************
+
+
+def unique_athletes_to_win_a_medal_after_40(df):
+
+    nbr_athletes = 99
+
+    print('The number of (unique) athletes who have won a medal after the age of 40 is: ' + str(nbr_athletes))
+
+
+# **************************************************************************
 
 
 def run_olympic_analysis():
 
-    youngest_olympian_medal(df)
+    df = pd.read_csv('olympic_data.csv')
+
+    youngest_olympian_to__receive_medal(df)
+    #city_with_lowest_nbr_medals_awarded(df)
+    #who_won_most_silver_medals(df)
+    #unique_athletes_to_win_a_medal_after_40(df)
+
+
+# **************************************************************************
 
 
 run_olympic_analysis()
+
+
+# **************************************************************************
+
