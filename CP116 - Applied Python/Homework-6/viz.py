87cc799faaf8f2cace90cdcbf9f127ac0b56536d
aidanprezzano <aidanprezzano@gmail.com>
2025-01-29 19:38:34 -0700

    updated gitignore

--- CP116 - Applied Python/Homework-6/viz.py
+++ CP116 - Applied Python/Homework-6/viz.py
@@ -16,11 +16,14 @@ def relationship_between_sport_and_age_in_olympics(df):
     print(gymnastic_age)
 
 
-    # plot the data by age
+    # plot the data by age, I choose lineplot because age is similar to comparing something over time. I think the line plot looks smooth
+    # for this type of data. I also added in title's so the reader understands fully what type of data is being presented. As you can see
+    # Gymnastics athletes can start very early, many having metals won at only 15 years old! The athletes reach their peak preformance around
+    # the age 24- 25 and then start to decline as they get older. I was still suprised that there were even few people winning medals above
+    # the age 35.
     sns.lineplot(gymnastic_age)
-    plt.title("The Medals per age")
+    plt.title("The Medals per age in Gymnastics")
     plt.ylabel('# Medals')
-    print('https://www.data-to-viz.com/#line')
     plt.show()
 
 
@@ -31,6 +34,20 @@ def relationship_between_sport_and_age_in_olympics(df):
 
 
 def how_has_happiness_changed_past_15_years(df):
+    country_1 = 'United States'
+
+    # only lines concerning USA
+    US = df[df["Country"] == country_1]
+
+    US_happiness = US.groupby('Life Ladder')['year'].max()
+
+    US_happiness_sorted = US_happiness.sort_values(ascending=False)
+
+    US_happiness_sorted_last_15 = [US_happiness_sorted.head(15)]
+    print(US_happiness_sorted_last_15)
+
+    sns.lineplot(US_happiness_sorted_last_15)
+    plt.show()
 
     print('how_has_happiness_changed_past_15_years')
 
@@ -53,13 +70,14 @@ def run_viz():
 
     print('------------------------------------------------------------\n')
 
-    relationship_between_sport_and_age_in_olympics(df_olympic)
+    #relationship_between_sport_and_age_in_olympics(df_olympic)
 
     print('\n------------------------------------------------------------\n')
 
-    #how_has_happiness_changed_past_15_years(df_happiness)
+    how_has_happiness_changed_past_15_years(df_happiness)
 
-    #print('\n------------------------------------------------------------\n')
+
+    print('\n------------------------------------------------------------\n')
 
     #relationship_between_wealth_generosity_and_happiness(df_happiness)
 
