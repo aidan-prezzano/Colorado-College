ab89419de76334ec624f9261ec3b2570884b301f
Charlie Prezzano <charlie@hawthornethreads.com>
2025-01-29 20:34:45 -0500

    hmwk7 - start

--- DaddyTime/CP116/Homework-7/viz.py
+++ DaddyTime/CP116/Homework-7/viz.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -6,7 +7,20 @@ import pandas as pd
 
 def relationship_between_sport_and_age_in_olympics(df):
 
-    print('relationship_between_sport_and_age_in_olympics')
+    sport = 'Gymnastics'
+    print('Studying how age impacts an athletes ability to medal in ' + sport + '\n')
+
+    # find all athletes for the sport
+    athletes_in_sport = df[(df['Sport'] == sport)]
+    print('There have been ' + str(len(athletes_in_sport)) + ' athletes and who have won a medal in ' + sport)
+
+    # group the athletes by age
+    nbr_athletes_by_age = athletes_in_sport.groupby(['Age']).size()
+    print(nbr_athletes_by_age)
+
+    for xyz in enumerate(nbr_athletes_by_age):
+        print(xyz)
+
 
 
 # **************************************************************************
@@ -39,11 +53,11 @@ def run_viz():
 
     print('\n------------------------------------------------------------\n')
 
-    how_has_happiness_changed_past_15_years(df_happiness)
+    #how_has_happiness_changed_past_15_years(df_happiness)
 
     print('\n------------------------------------------------------------\n')
 
-    relationship_between_wealth_generosity_and_happiness(df_happiness)
+    #relationship_between_wealth_generosity_and_happiness(df_happiness)
 
     print('\n------------------------------------------------------------')
 
