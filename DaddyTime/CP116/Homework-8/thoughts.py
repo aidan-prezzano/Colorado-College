c1c5b6979c9e1cdddc050b808376bffd231da439
Charlie Prezzano <charlie@hawthornethreads.com>
2025-02-03 13:38:40 -0500

    hmwk8 - started thoughts

--- DaddyTime/CP116/Homework-8/thoughts.py
+++ DaddyTime/CP116/Homework-8/thoughts.py
@@ -28,29 +28,43 @@ def run_thoughts():
     # split the data into training and test data
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=TRAIN_TEST_SPLIT_TEST_SIZE, random_state=TRAIN_TEST_SPLIT_RANDOM_STATE)
 
+    # define a model, then fit it
     model = LinearRegression()
     model.fit(X_train, Y_train)
 
-    # predicting on the X_test data set
-    print(model.predict(X_test))
-
-    # summary of the model
-    print('model intercept :', model.intercept_)
+    # display the summary of the model
     print('model coefficients : ', model.coef_)
     print('Model score : ', model.score(X, Y))
 
+
+    # create predictions on test data
     Y_pred = model.predict(X_test)
 
-    #print(Y_test)
-    #print(Y_pred)
+    # create residuals,
+    # if > 0, then prediction was higher than actual
+    # if < 0, then prediction was lower than actual
+    residuals = Y_pred - Y_test
 
-    residuals = Y_test - Y_pred
+    print('')
+    print('Y Test[0]:')
+    print(Y_test.head(1))
+    print('')
+    print('Y Pred[0]:')
+    print(Y_pred[0])
+    print('')
+    print('Residuals[0]:')
+    print(residuals.head(1))
+
+    # plt out the residuals values
     plt.scatter(Y_pred, residuals)
-    plt.axhline(y=0, color='r', linestyle='-')
+    plt.axhline(y=0, color='green', linestyle='-')
+    plt.axhline(y=-1, color='red', linestyle='-')
+    plt.axhline(y=1, color='red', linestyle='-')
     plt.xlabel("Predicted Values")
     plt.ylabel("Residuals")
     plt.title("Residual Plot")
     plt.show()
+
 
 
 
